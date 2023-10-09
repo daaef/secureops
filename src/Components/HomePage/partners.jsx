a1234812373f21723c229485fa89db4b93a0c751
Robinson Simon <cf.youvarsity@gmail.com>
2023-10-09 08:04:43 +0100

    edited the padding between

--- src/Components/HomePage/partners.jsx
+++ src/Components/HomePage/partners.jsx
@@ -8,7 +8,7 @@ import netflix from "../../Assets/images/netflix.png";
 const Partners = () => {
   const logos = [google, airbnb, netflix, amazon, adobe];
   return (
-    <div className='mx-auto max-w-5xl my-10 p-4'>
+    <div className='mx-auto max-w-5xl my-20 p-4'>
       <h2 className='text-center text-lg'>Clients we've worked with</h2>
       <div className='mt-5 grid grid-cols-5 gap-4'>
         {logos.map((logo, ind) => (
