6896b9f9f3253b23bfb60533c6c9f3a8f4a6245c
chosen2730 <cf.youvarsity@gmail.com>
2023-11-14 20:40:27 +0100

    added services page

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Hero from "../Components/Services/hero";
+import Ctx from "../Components/HomePage/cta";
+import Allservices from "../Components/Services/services";
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div>
+      <Hero />
+      <Allservices />
+      <Ctx />
+    </div>
+  );
 };
 
 export default Services;
