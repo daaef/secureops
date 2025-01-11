3f4bc2914696c4ebc7aa8ff1b426cfb93c8cd392
Afekhide Gbadamosi <realafe21@gmail.com>
2025-01-12 00:26:51 +0100

    clear project

--- src/hooks/scrollToTop.jsx
+++
@@ -1,9 +0,0 @@
-import { useLayoutEffect } from "react";
-
-const useScrollToTop = () => {
-  useLayoutEffect(() => {
-    window.scrollTo({ behavior: "smooth", top: 0 });
-  }, []);
-};
-
-export default useScrollToTop;
