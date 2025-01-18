07fecd03d19da64e5ef2f8ddc349dc306b43eb00
Afekhide Gbadamosi <realafe21@gmail.com>
2025-01-18 20:19:42 +0100

    Remove legal disclaimer from contact page and streamline content presentation

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -99,11 +99,6 @@ function ContactDetails() {
       <h2 className="font-display text-base font-semibold text-primary-950">
         Our offices
       </h2>
-      <p className="mt-6 text-base text-primary-600">
-        Prefer doing things in person? We don’t but we have to list our
-        addresses here for legal reasons.
-      </p>
-
       <Offices className="mt-10 grid grid-cols-1 gap-8 sm:grid-cols-2" />
 
       <Border className="mt-16 pt-16">
